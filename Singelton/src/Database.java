01db400b3c6c1a5f880175fea72af3053c749e4e
Othman Alkhamra <oalkhamra@pivotal.io>
2020-02-10 15:56:45 +0000

    add sleep between creations

--- Singelton/src/Database.java
+++ Singelton/src/Database.java
@@ -1,9 +1,15 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Database {
 
     private static Database database;
     private static Object _lockObject = new Object();
+    private Date createdAt;
 
     private Database() {
+        createdAt = new Date();
         System.out.println("Constructor has been called!");
     }
 
@@ -14,5 +20,10 @@ public class Database {
             }
         }
         return database;
+    }
+
+    public void printDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        System.out.println(dateFormat.format(createdAt)); //2016/11/16 12:08:43
     }
 }
--- Singelton/src/Main.java
+++ Singelton/src/Main.java
@@ -1,8 +1,13 @@
-public class Main {
-    public static void main(String [] args) {
-        Database database = Database.getInstance();
+import java.io.InterruptedIOException;
 
+public class Main {
+    public static void main(String [] args)  throws Exception
+    {
+        Database database = Database.getInstance();
+        database.printDate();
+        Thread.sleep(4000);
         Database database2 = Database.getInstance();
+        database2.printDate();
 
     }
 }
